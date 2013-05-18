ae1095fbf848c4b407e56d93c90ec38bb78a4adc
Alex Rozumey <brain-geek@yandex.ua>
2013-05-18 18:30:32 +0300

    Minor changes - fix usage of vmbuilder.cfg

--- vmfiles/createvm.sh
+++ vmfiles/createvm.sh
@@ -4,7 +4,7 @@ IP=$2
 
 echo "Creating VM with hostname: $HOSTNAME and ip: $IP"
 
-vmbuilder kvm ubuntu -d /virt/$HOSTNAME --ip $IP --hostname $HOSTNAME
+vmbuilder kvm ubuntu -d /virt/$HOSTNAME --ip $IP --hostname $HOSTNAME -c vmbuilder.cfg
 virsh start $HOSTNAME
 
 echo "Done."
