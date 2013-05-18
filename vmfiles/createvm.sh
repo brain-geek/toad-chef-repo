70a76cc0c0e8e50421bab1c9548ecf3696020ec2
Alex Rozumey <brain-geek@yandex.ua>
2013-05-18 18:43:46 +0300

    It does not get login and passwd from .cfg - moving it to cli params

--- vmfiles/createvm.sh
+++ vmfiles/createvm.sh
@@ -4,7 +4,7 @@ IP=$2
 
 echo "Creating VM with hostname: $HOSTNAME and ip: $IP"
 
-vmbuilder kvm ubuntu -d /virt/$HOSTNAME --ip $IP -o --hostname $HOSTNAME -c vmbuilder.cfg
+vmbuilder kvm ubuntu -d /virt/$HOSTNAME --ip $IP -o --hostname $HOSTNAME -c vmbuilder.cfg --user brain --name user --pass password
 virsh start $HOSTNAME
 
 echo "Done."
