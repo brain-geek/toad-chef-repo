8b9ccfcc05b372294e8bfa064471bd4028c7baf2
Alex Rozumey <brain-geek@yandex.ua>
2013-05-21 11:04:49 +0300

    Change attributes

--- cookbooks/lttapp/attributes/default.rb
+++ cookbooks/lttapp/attributes/default.rb
@@ -1,6 +1,9 @@
 default[:application_path] = "/var/www/ltt"
 default[:unicorn_port] = '8080'
 
-default['rvm']['installer_url'] = "https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer" 
-default['rvm']['user_installs'] = [ { :user => "brain" } ]
-default['rvm']['user_rubies'] = [ "1.9.3" ]
+override['rvm']['user_installs'] = [ { :user => "brain" } ]
+override['rvm']['user_rubies'] = [ "1.9.3" ]
+
+override['rvm']['version'] =          "1.17.10"
+override['rvm']['installer_url'] =    "https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer"
+override['rvm']['branch'] =           "none"
