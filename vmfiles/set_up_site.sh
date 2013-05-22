7b690b8e52bbb32fafb8e3a8c408b4860ad39eb9
Alex Rozumey <brain-geek@yandex.ua>
2013-05-22 18:52:42 +0300

    Change hostname in bootstraping the node.

--- vmfiles/set_up_site.sh
+++ vmfiles/set_up_site.sh
@@ -8,4 +8,4 @@ sudo ./createvm.sh $HOSTNAME $IP
 
 sleep 30
 
-knife bootstrap $IP -x brain -N $HOSTNAME-toad -P password -r 'role[simple_webserver]' --sudo
+knife bootstrap $IP -x brain -N $HOSTNAME -P password -r 'role[simple_webserver]' --sudo
