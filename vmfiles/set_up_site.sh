c56aaba2dac75efd2714075412b61f476ff7d5cd
Alex Rozumey <brain-geek@yandex.ua>
2013-05-22 18:58:47 +0300

    Adding role by knife cli.

--- vmfiles/set_up_site.sh
+++ vmfiles/set_up_site.sh
@@ -8,4 +8,7 @@ sudo ./createvm.sh $HOSTNAME $IP
 
 sleep 30
 
-knife bootstrap $IP -x brain -N $HOSTNAME -P password -r 'role[simple_webserver]' --sudo
+knife bootstrap $IP -x brain -N $HOSTNAME -P password --sudo
+
+knife node run_list add $HOSTNAME 'role[simple_webserver]'
+knife ssh name:$HOSTNAME "sudo chef-client" -x brain -a ipaddress -P password
