2f7d8ddab3b5c367fdef17e5a993fbf7c9299404
Alex Rozumey <brain-geek@yandex.ua>
2013-05-22 18:14:43 +0300

    Minor change - moving set_up_user

--- vmfiles/set_up_site.sh
+++ vmfiles/set_up_site.sh
@@ -4,5 +4,5 @@ IP=$2
 
 echo 'Run me as user, please!'
 
-sudo vmfiles/createvm.sh $HOSTNAME $IP
+sudo ./createvm.sh $HOSTNAME $IP
 knife bootstrap $IP -x brain -N $HOSTNAME-toad -P password -r 'role[simple_webserver]' --sudo
