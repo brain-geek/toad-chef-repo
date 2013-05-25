cf089b598f72725aa94bc2e4c90933d8937abe0c
Alex Rozumey <brain-geek@yandex.ua>
2013-05-25 22:01:29 +0300

    Starting VM only if it has been created.

--- vmfiles/createvm.sh
+++ vmfiles/createvm.sh
@@ -4,7 +4,6 @@ IP=$2
 
 echo "Creating VM with hostname: $HOSTNAME and ip: $IP"
 
-vmbuilder kvm ubuntu -d /virt/$HOSTNAME --ip $IP -o --hostname $HOSTNAME -c vmbuilder.cfg --user brain --name user --pass password
-virsh start $HOSTNAME
+vmbuilder kvm ubuntu -d /virt/$HOSTNAME --ip $IP -o --hostname $HOSTNAME -c vmbuilder.cfg --user brain --name user --pass password && virsh start $HOSTNAME
 
 echo "Done."
